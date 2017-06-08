45731e7729774c2656084628b399667edb55ab3d
John Clements <clements@racket-lang.org>
2017-06-08 13:20:56 -0700

    setting up collection dir

---
+++ info.rkt
@@ -0,0 +1,19 @@
+#lang setup/infotab
+
+(define collection 'multi)
+
+(define deps
+  '()
+  #;(list "base"
+        "rackunit-lib"
+        "scribble-lib"
+        "typed-racket-lib"
+        "typed-racket-more"
+        "web-server-lib"
+        "pfds"))
+
+(define build-deps
+  '()
+  #;(list "rackunit-lib"
+        "racket-doc"))
+
