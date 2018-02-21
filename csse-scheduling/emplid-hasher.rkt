835bd4d9bb1bbf248a0ed9a5f0600d0156e79bbc
John Clements <clements@racket-lang.org>
2018-02-21 11:21:52 -0800

    adjust to handle trimmed emplids

--- csse-scheduling/emplid-hasher.rkt
+++ csse-scheduling/emplid-hasher.rkt
@@ -4,6 +4,11 @@
 ;; stored on the Cal Poly OneDrive. It appears that the chance of
 ;; a collision, for 100K students, is less than 4e-15.
 
+;; note that cal poly can be a little inconsistent; Emplids are sometimes
+;; treated as strings, and sometimes treated as integers, in which case
+;; leading zeros may be trimmed, yikes. To fix this, we pad strings
+;; shorter than the expected value to 9 digits.
+
 (provide emplid->hashed)
 
 (require/typed sha
@@ -29,16 +34,33 @@ University/student-data-hash/student-hash-key.txt")))
 ;; I'm ignoring the possibility of collisions.
 
 (define (emplid->hashed [emplid : String])
+  (define padding-string
+    (cond [(<= (string-length emplid) 9)
+           (apply string-append
+                  (build-list (- 9 (string-length emplid)) (λ (_) "0")))]
+          [else (raise-argument-error
+                 'emplid->hashed
+                 "string of length <= 9"
+                 0 emplid)]))
   (apply
    string-append
    (map hexpair
         (bytes->list (subbytes
                       (sha256
-                       (string->bytes/utf-8 (string-append emplid hash-text)))
+                       (string->bytes/utf-8
+                        (string-append padding-string emplid hash-text)))
                       0 NUM-BYTES)))))
 
-(unless (equal? (emplid->hashed "01234")
-                "46bcbd81d771acb20ef1")
-  (error 'ohdear))
+(module+ test
+  (require typed/rackunit)
+
+  (check-equal? (emplid->hashed "003911748")
+                "926ae4efd422daaa2afe")
+  
+  (check-equal? (emplid->hashed "01234")
+                "b6e25c191fb6d111cc3a")
+
+  (check-equal? (emplid->hashed "1234")
+          (emplid->hashed "000001234")))
 
 
