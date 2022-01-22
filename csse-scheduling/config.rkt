7edf1424c8f98f57080c1f04eac35f87d9f41d80
John Clements <clements@racket-lang.org>
2022-01-21 17:18:18 -0800

    config repair

--- csse-scheduling/config.rkt
+++ csse-scheduling/config.rkt
@@ -5,12 +5,18 @@
 (require racket/runtime-path)
 (define-runtime-path here ".")
 
-(define config-sexp
-  (cast (file->value (build-path here "config-prvt.rktd"))
-        (Listof (Pairof Symbol (U Natural String)))))
+(define-type ConfigContent (Listof (Pairof Symbol (U Natural String))))
+(define-predicate config-content? ConfigContent)
+
+(define config-sexp : ConfigContent
+  (let ()
+    (define pre-content (file->value (build-path here "config-prvt.rktd")))
+    (cond [(config-content? pre-content) pre-content]
+          [else (error 'config-sexp "expected config content, got ~e"
+                       pre-content)])))
 
 (define (config-env [tag : Symbol])
   (match (assoc tag config-sexp)
     [(cons _ val) val]
-    [#f (error 'config-env "no config var with tag ~v")]))
+    [#f (error 'config-env "no config var with tag ~v" tag)]))
 
