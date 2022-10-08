3352165ffa685adadc872314a41aacf238c7aab8
John Clements <clements@racket-lang.org>
2022-10-07 17:42:37 -0700

    comment out some code

--- csse-scheduling/fetch-mapping.rkt
+++ csse-scheduling/fetch-mapping.rkt
@@ -9,8 +9,10 @@
          majors-cache
          2188-1-student-grades
          2188-1-majors
-         active-faculty
-         offerings)
+         ;; just use dept-faculty-offerings instead:
+         ;active-faculty
+         ;offerings
+         )
 
 
 (require db
@@ -146,7 +148,9 @@
 
 ;; extending yet again for the who-does-what queries...
 
-(define (active-faculty qtr)
+;; actually, adding this was arguably a mistake... should be combined with dept-faculty-offerings.
+
+#;(define (active-faculty qtr)
   (cache-query
    "fad"
    (~a "active-faculty-" qtr ".withcache")
@@ -161,7 +165,8 @@
           ";")
       qtr))))
 
-(define (offerings last-qtr)
+
+#;(define (offerings last-qtr)
   (cache-query
    "fad"
    (~a "offerings-up-through-" last-qtr ".withcache")
