08c94bccf3ad6617a0e3393b769b84146e704133
John Clements <clements@racket-lang.org>
2023-02-14 17:41:53 -0800

    updated coordinators, also format

--- csse-scheduling/coordinators.rkt
+++ csse-scheduling/coordinators.rkt
@@ -5,6 +5,16 @@
 ;; list of instructor last names. Courses with no coordinator
 ;; are associated with empty lists.
 
+;; the file should be a CSV file, with three columns.
+;; the first column is a course name, written either as
+;; e.g. "CSC 442" or "CSC/CPE 451"
+;; the second column is the name of the course. This might not be checked....
+;; the third column is either the string representation of a value in
+;; (Listof (U Symbol String (List Symbol)))
+;; e.g. "(Clements (csse-dept-chair))"
+;; or a string that doesn't start with a paren, in which case it's
+;; just a last name e.g. "Stanchev"
+
 ;; checks to make sure no course appears more than once. Does not
 ;; check that all courses are listed (see stale-coordinators.rkt)
 
@@ -12,7 +22,8 @@
                [csv->list (Input-Port -> (Listof (Listof Any)))])
 
 (require "canonicalize.rkt"
-         racket/runtime-path)
+         racket/runtime-path
+         typed/rackunit)
 
 (define-runtime-path here ".")
 
@@ -22,10 +33,18 @@
 ;; instructors that are the coordinators for that course
 (provide coordinators)
 
-(define-type CoordinatorRecord (List String (Listof String)))
+;; contains a canonical course name and a list of names & designators (either last names or special
+;; identifiers like 'cpe-dept-chair
+(define-type CoordinatorRecord (List String (Listof (U String Symbol))))
+(define-type SpecialCoordinator (U 'csse-dept-chair
+                                   'csse-grad-coordinator
+                                   'cpe-dept-chair
+                                   'ee-dept-chair))
+
 (define-type CoordinatorList (Listof CoordinatorRecord))
 
 (define-predicate lostrstrstr? (Listof (List String String String)))
+(define-predicate special-coordinator? SpecialCoordinator)
 
 (define raw-coordinators
   (call-with-input-file (build-path here "coordinators.csv") csv->list))
@@ -36,7 +55,37 @@
                      "expected csv where each line has 3 strings, got: ~e"
                      raw-coordinators)]))
 
-(define (coordinators [cc : CatalogCycle])
+;; read a single value from a string, ensure that it's empty afterawrd
+(define (string->value [s : String])
+  (define ip (open-input-string s))
+  (define r (read ip))
+  (define leftover (port->string ip))
+  (unless (equal? (string-trim leftover) "")
+    (error 'string->value "leftover non-blank text: ~e" leftover))
+  r)
+
+
+(define (parse-coordinators [str : String]) : (Listof (U String Symbol))
+  (match str
+    [(and s (regexp #px"^\\("))
+     (define v (string->value s))
+     (cond [(list? v)
+            (for/list ([e v])
+              (match e
+                [(? symbol? e) (symbol->string e)]
+                [(? string? s) s]
+                [(list (? special-coordinator? sc)) sc]
+                [other (error 'parse-coordinators "expected legal coordinator, got ~e"
+                              e)]))]
+           [else (error 'parse-coordinators "internal error 117729837018")])]
+    [else (list str)]))
+
+(check-equal? (parse-coordinators "Knight") '("Knight"))
+(check-equal? (parse-coordinators "(Knight Queen)") '("Knight" "Queen"))
+(check-equal? (parse-coordinators "(Knight (csse-dept-chair) Queen)") '("Knight" csse-dept-chair "Queen"))
+(check-equal? (parse-coordinators "(\"biggy biggy biggy\")") '("biggy biggy biggy"))
+
+(define (coordinators [cc : CatalogCycle]) : CoordinatorList
   (define coordinators-list
     (for/list : CoordinatorList ([l (in-list coordinator-strs)])
       (define canonical-name
@@ -62,10 +111,7 @@ courses: ~e"
                               subj)]
                  )]))
       (define coordinator
-        (match l
-          [(list _ _ name)
-           (regexp-split #px"/" name)]
-          [(list _ _ "") #f]))
+        (parse-coordinators (third l)))
       (list canonical-name coordinator)))
   ;; ensure that no course is listed more than once:
   (define possible-duplicate (check-duplicates (map (inst first String) coordinators-list)))
