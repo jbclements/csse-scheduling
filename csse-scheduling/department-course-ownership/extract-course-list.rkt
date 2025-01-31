72fe9e9ebe7b766f4a2a564672f619f6ed364750
John Clements <clements@racket-lang.org>
2025-01-30 16:33:55 -0800

    ownership

--- csse-scheduling/department-course-ownership/extract-course-list.rkt
+++ csse-scheduling/department-course-ownership/extract-course-list.rkt
@@ -5,6 +5,13 @@
          sxml
          )
 
+;; this file extracts course names from the CLSS course lists. To obtain the full
+;; list for (e.g.) the CPE department, go to the URL
+;; https://nextcatalog-admin.calpoly.edu/wen/2254/184-CPE/?osu=184-CPE
+;; (side note, I see this is under nextcatalog, I could probably script marionette here...)
+;; then click "show courses with no sections", then save the page as HTML, then
+;; use the code below.
+
 (define sxml
 (call-with-input-file
     "/tmp/abc.html"
@@ -12,12 +19,12 @@
   html->xexp
  ))
 
-;; fragile much?
+;; This is SO FRAGILE
 (define the-rows
   ((sxpath '(// (div (@ id (equal? "offering-grid"))) table tbody tr td div))
    sxml))
 
-(explore the-rows)
+;(explore the-rows)
 
 (map second the-rows)
 
--- csse-scheduling/ownership.rkt
+++ csse-scheduling/ownership.rkt
@@ -1,81 +1,90 @@
 #lang racket
 
+;; better way, I think:
+
+(require "./department-course-ownership/course-list.rkt")
+(provide (all-from-out "./department-course-ownership/course-list.rkt"))
+
+;; old code:
 ;; right now, this just parses the list of CPE-owned courses.
-
-(require racket/runtime-path
-         csv-reading
-         sugar
-         "discard-bom.rkt"
-         csse-scheduling/canonicalize)
-
-(provide 2022-cpe-courses)
-
-(define-runtime-path here ".")
-
-(define cc "2022-2026")
-
-(define table
-  (call-with-input-file (build-path here "CPE-housed courses.csv") 
-    (λ (port)
-      (csv->list (discard-bom port)))))
-
-(when (not (equal? '(1) (remove-duplicates (map length table))))
-  (error 'table-shape
-         "expected a single column"))
-
-(define cells (map first table))
-
-(define groups
-  (map
-   (λ (g)
-     (cond [(and (pair? g) (equal? (first g) ""))
-            (rest g)]
-           [else g]))
-   (slicef-at cells (λ (c) (equal? c "")))))
-
-(define titles
-  (map first groups))
-
-;; NB ligatures or other nonstandard characters in here, sigh...
-;; if this fails, make sure that the new value of titles doesn't
-;; contain any courses, and just reset it. This is essentially just
-;; a regression test.
-(unless (equal? titles
-                '("CPE-housed Courses"
-                  "Orientation"
-                  "Architecture, Parallel, and Distributed Computing"
-                  "Embedded Systems"
-                  "Computer Networks"
-                  "Robotics"
-                  "Ethical Computing, Privacy, and Security"
-                  "Design"
-                  "Graduate Courses"
-                  "CPE Special Topics / Project Courses "))
-  (error 'title-check "unexpected titles"))
+#;(module unused racket
+  (require racket/runtime-path
+           csv-reading
+           sugar
+           "discard-bom.rkt"
+           csse-scheduling/canonicalize)
 
 
 
-(define (parse-line s)
-  (match-define (list _ pre num post) (regexp-match #px"^(.*)([[:digit:]]{3})(.*)$" s))
-  (define subjects
-    (match (string-trim pre)
-      [(or "CPE" "CSC" "EE") (list (string-trim pre))]
-      [(regexp #px"^([A-Z]{2,3})\\(([A-Z]{2,3})\\)" (list _ a b))
-       (list a b)]      
-      [other (error "unexpected prefix: ~e" other)]))
-  (list subjects (string->number (string-trim num))))
 
-(define (my-canonicalize subjects num)
-  (remove-duplicates
-   (map (λ (subject) (canonicalize cc subject num))
-        subjects)))
+  (provide 2022-cpe-courses)
 
-(define courses
-  (map (λ (pr) (apply my-canonicalize pr))
-       (map parse-line (apply append (map rest groups)))))
+  (define-runtime-path here ".")
 
-(unless (equal? '(1) (remove-duplicates (map length courses)))
-  (error 'courses "two courses listed as one. add info here"))
+  (define cc "2022-2026")
 
-(define 2022-cpe-courses
-  (apply append courses))
+  (define table
+    (call-with-input-file (build-path here "CPE-housed courses.csv") 
+      (λ (port)
+        (csv->list (discard-bom port)))))
+
+  (when (not (equal? '(1) (remove-duplicates (map length table))))
+    (error 'table-shape
+           "expected a single column"))
+
+  (define cells (map first table))
+
+  (define groups
+    (map
+     (λ (g)
+       (cond [(and (pair? g) (equal? (first g) ""))
+              (rest g)]
+             [else g]))
+     (slicef-at cells (λ (c) (equal? c "")))))
+
+  (define titles
+    (map first groups))
+
+  ;; NB ligatures or other nonstandard characters in here, sigh...
+  ;; if this fails, make sure that the new value of titles doesn't
+  ;; contain any courses, and just reset it. This is essentially just
+  ;; a regression test.
+  (unless (equal? titles
+                  '("CPE-housed Courses"
+                    "Orientation"
+                    "Architecture, Parallel, and Distributed Computing"
+                    "Embedded Systems"
+                    "Computer Networks"
+                    "Robotics"
+                    "Ethical Computing, Privacy, and Security"
+                    "Design"
+                    "Graduate Courses"
+                    "CPE Special Topics / Project Courses "))
+    (error 'title-check "unexpected titles"))
+
+
+
+  (define (parse-line s)
+    (match-define (list _ pre num post) (regexp-match #px"^(.*)([[:digit:]]{3})(.*)$" s))
+    (define subjects
+      (match (string-trim pre)
+        [(or "CPE" "CSC" "EE") (list (string-trim pre))]
+        [(regexp #px"^([A-Z]{2,3})\\(([A-Z]{2,3})\\)" (list _ a b))
+         (list a b)]      
+        [other (error "unexpected prefix: ~e" other)]))
+    (list subjects (string->number (string-trim num))))
+
+  (define (my-canonicalize subjects num)
+    (remove-duplicates
+     (map (λ (subject) (canonicalize cc subject num))
+          subjects)))
+
+  (define courses
+    (map (λ (pr) (apply my-canonicalize pr))
+         (map parse-line (apply append (map rest groups)))))
+
+  (unless (equal? '(1) (remove-duplicates (map length courses)))
+    (error 'courses "two courses listed as one. add info here"))
+
+  (define 2022-cpe-courses
+    (apply append courses)))
